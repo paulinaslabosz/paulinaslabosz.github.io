3537f9f77c22d2d75368ce0105ae60c0292bd4e1
Paulina Słabosz <sejmipool@gmail.com>
2021-07-13 14:22:16 +0200

    javascript projects feature

--- src/js/index.js
+++ src/js/index.js
@@ -6,4 +6,57 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+fetch("https://api.github.com/users/paulinaslabosz/repos?sort=created&direction=asc")
+  .then((res) => res.json())
+  .then((res) => {
+    const container = document.querySelector('.projects__grid--js');
+    for (let repo of res) {
+        const {description, homepage, html_url, name} = repo;
+
+        const template = `<article class="project">
+        <div class="project__window">
+          <span class="project__circle"></span>
+          <span class="project__circle"></span>
+          <span class="project__circle"></span>
+        </div>
+        <div class="project__content">
+          <img src="img/github.svg" alt="" class="project__icon" />
+          <h3 class="project__grid">
+            <span class="project__label">project:</span
+            ><span class="project__title">${name}</span>
+          </h3>
+          <p class="project__grid project__grid--description">
+            <span class="project__label">description:</span
+            ><span class="project__description"
+              >${description}</span
+            >
+          </p>
+          <p class="project__grid">
+            <span class="project__label">demo:</span
+            ><span
+              >&lt;<a
+                href="${homepage}"
+                title="${name} - demo"
+                class="project__link"
+                >see_here</a
+              >&gt;</span
+            >
+          </p>
+          <p class="project__grid">
+            <span class="project__label">github:</span
+            ><span
+              >&lt;<a
+                href="${html_url}"
+                title="${name} - github"
+                class="project__link"
+                >source_code</a
+              >&gt;</span
+            >
+          </p>
+        </div>
+      </article>`;
+        if (description) {container.innerHTML += template};
+      
+    }
+  })
+  .catch((e) => console.log(e));
