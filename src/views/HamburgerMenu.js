97035ad87d01203594cc9d23708ba89efd7e297a
Paulina Słabosz <sejmipool@gmail.com>
2025-05-22 22:17:22 +0200

    Media: add useState fo opening ham menu

--- src/views/HamburgerMenu.js
+++ src/views/HamburgerMenu.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import { React, useState } from 'react';
 import { Wrapper, HamIcon } from './HamburgerMenu.styles';
 import HamburgerIcon from '../assets/burgermenu.svg';
 
 function HamburgerMenu() {
+  const [isOpen, setIsOpen] = useState(false);
+  console.log(isOpen);
   return (
     <Wrapper>
-      <HamIcon width="800px" height="800px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+      <HamIcon onClick={() => setIsOpen(!isOpen)} width="800px" height="800px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
         <path d="M4 18L20 18" stroke="currentColor" strokeWidth="2" strokeLinecap="round" />
         <path d="M4 12L20 12" stroke="currentColor" strokeWidth="2" strokeLinecap="round" />
         <path d="M4 6L20 6" stroke="currentColor" strokeWidth="2" strokeLinecap="round" />
