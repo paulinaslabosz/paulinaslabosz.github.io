e966acdc3a0cc78efea2f4057298e2df5a48c384
Paulina Słabosz <sejmipool@gmail.com>
2025-05-25 22:28:27 +0200

    Media: fixed - height expands with content

--- src/views/Root.styles.js
+++ src/views/Root.styles.js
@@ -3,7 +3,7 @@ import backgroundPhoto from '../assets/background.jpg';
 
 export const Background = styled.div`
   width: 100%;
-  height: 100vh;
+  min-height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
