f991dbccf1039abd68e6cb33e13f7fb6e6e7a0b1
Paulina Słabosz <sejmipool@gmail.com>
2025-06-03 21:54:59 +0200

    Fixed: problem with API rate limit, add static json file with projects

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,34 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import Project from '../components/Project';
 import { Title, List } from './Projects.styles';
+import projects from '../assets/repos.json';
+
 function Projects() {
-  const [projects, setProjects] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  const api_path = 'https://api.github.com/users/paulinaslabosz/repos?sort=created';
-
-  const cacheKey = 'reposCache';
-  const cacheTime = 1000 * 60 * 10;
-
-  const getProjects = async () => {
-    const cached = JSON.parse(localStorage.getItem(cacheKey));
-    const timeNow = Date.now();
-    if (cached && timeNow - cached.timestamp < cacheTime) {
-      setProjects(cached.data);
-      setLoading(false);
-      return;
-    }
-    const response = await fetch(api_path);
-    const data = await response.json();
-    localStorage.setItem(cacheKey, JSON.stringify({ data, timestamp: timeNow }));
-    setProjects(data);
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    getProjects();
-  }, []);
-
   const projectsList = projects.map((project) => (
     <Project
       key={project.id}
@@ -41,6 +16,7 @@ function Projects() {
     />
   ));
 
+  console.log(projects);
   return (
     <>
       <Title>Projects</Title>
